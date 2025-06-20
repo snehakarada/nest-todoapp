ffaf2f26f9caeec60aa52284594f025415148c6b
Bhagya <146413665+snehakarada@users.noreply.github.com>
2025-06-20 17:25:30 +0530

    removed the gh action file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
-import { Body, Injectable, InjectableOptions, NestMiddleware, Res } from '@nestjs/common';
+import { Body, Injectable, NestMiddleware, Res } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
+        console.log(`${req.method} ${req.originalUrl}`);
         next();
     }
 
